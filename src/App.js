93539335ebb4516ef2a325b754824b60f79c4a08
unknown <hpcgg@foxmail.com>
2019-12-26 17:22:55 +0800

    an other router

--- src/App.js
+++ src/App.js
@@ -2,28 +2,28 @@ import React from 'react';
 import './App.css';
 import './styles/frame.css';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-import Publish from './components/view/publish';
+import Publish from './components/view/Publish';
 import Content from './components/layout/Content';
 import Header from './components/layout/Header';
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <Router>
-          <Switch>
-            <Header></Header>
-            <section className="content">
-              <Route path="/">
+      <Router>
+        <div className="App">
+          <Header></Header>
+          <section className="content">
+            <Switch>
+              <Route exact path="/">
                 <Content />
               </Route>
-              <Route path="/publish">
+              <Route exact path="/publish">
                 <Publish />
               </Route>
-            </section>
-            <footer className="footer">@ICHIKO</footer>
-          </Switch>
-        </Router>
-      </div>
+            </Switch>
+          </section>
+          <footer className="footer">@ICHIKO</footer>
+        </div>
+      </Router>
     );
   }
 }
