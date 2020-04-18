3f010a202d2352e072a5e233f1a472dcf3612b8d
hikari8866 <hpcgg@foxmail.com>
2020-04-18 15:11:04 +0800

    上传打包后的文件

---
+++ build/precache-manifest.1262999a4e4f1866e211fc080831ba76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c9868be8529d1e1df2f204b76a7b809",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a3d6cef382e47eaf538",
+    "url": "/static/css/2.7f669fb7.chunk.css"
+  },
+  {
+    "revision": "ebfa1e720c1a9f1ce370",
+    "url": "/static/css/main.10aae8b0.chunk.css"
+  },
+  {
+    "revision": "0a3d6cef382e47eaf538",
+    "url": "/static/js/2.8733ba48.chunk.js"
+  },
+  {
+    "revision": "ebfa1e720c1a9f1ce370",
+    "url": "/static/js/main.7cdf0fbc.chunk.js"
+  },
+  {
+    "revision": "6ca63645db554e185cd7",
+    "url": "/static/js/runtime-main.86a044b4.js"
+  }
+]);
--- src/components/Editor/editor.js
+++ src/components/Editor/editor.js
@@ -114,8 +114,7 @@ class Editorx extends React.Component {
         insertImg(url);
         // result 必须是一个 JSON 格式字符串！！！否则报错
       }
-    };
-    editor.create();
+    };    editor.create();
   };
   render() {
     return (
